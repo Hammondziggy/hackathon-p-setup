be863444f7d19d650ff4d6e3325b4b2f219837ee
Hammondziggy <zorzorisrael@gmail.com>
2023-08-24 02:10:45 +0000

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
     './src/components/header/**/*.{js,jsx}',
     './src/components/body/**/*.{js,jsx}',
-    './src/components/footer/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-    './public/index.html'],
+    './src/components/footer/**/*.{js,jsx}'
+  ],
   theme: {
     extend: {},
   },
